d1330035fb95785c919babc342ff03a2db41ac3f
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-05-13 13:35:21 +0700

    commit#4

--- src/main/java/YouTrackApiCreateProjects.java
+++ src/main/java/YouTrackApiCreateProjects.java
@@ -1,3 +1,37 @@
+import io.qameta.allure.Step;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 public class YouTrackApiCreateProjects {
 
+    private final String BASE_URI;
+    private final String END_POINT_ONE;
+    private final String END_POINT_TWO;
+    private final String END_POINT_THREE;
+
+    public YouTrackApiCreateProjects(String BASE_URI, String END_POINT_ONE, String END_POINT_TWO, String END_POINT_THREE){
+        this.BASE_URI = BASE_URI;
+        this.END_POINT_ONE = END_POINT_ONE;
+        this.END_POINT_TWO = END_POINT_TWO;
+        this.END_POINT_THREE = END_POINT_THREE;
+    }
+
+    public void setUp(){
+        RestAssured.baseURI = BASE_URI;
+    }
+
+    @Step("Отправляем наименование проекта и его идентификатор")
+    public Response postNameAndShortName(){
+
+    }
+
+    @Step("Отправляем настройки проекта")
+    public Response postProjectSettings(){
+
+    }
+
+    @Step("Завершающий запрос на создание проекта")
+    public Response postTheFinalRequest(){
+
+    }
 }
