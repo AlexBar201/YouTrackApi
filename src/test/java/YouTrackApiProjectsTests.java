a0cfe9f80c8aaa1d8bb3726163000bb6ac75a355
Aleksandr Baranov <alexmaksimum54@yandex.ru>
2025-05-14 13:16:47 +0700

    commit#12

--- src/test/java/YouTrackApiProjectsTests.java
+++ src/test/java/YouTrackApiProjectsTests.java
@@ -1,15 +1,21 @@
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class YouTrackApiProjectsTests {
 
+    static YouTrackApiProjects youTrack = new YouTrackApiProjects();
+
+    @BeforeAll
+    static void globalSetUp(){
+        youTrack.setUp();
+    }
+
     @Nested
-    class PostAndPointTests{
-        YouTrackApiProjects youTrack = new YouTrackApiProjects();
+    class PostAndPointsTests {
         @Test
         public void testEndPoint(){
-            youTrack.setUp();
             Response firstAndPoint = youTrack.postNameAndShortName("Project1488", "wer");
             firstAndPoint.then().statusCode(200);
         }
